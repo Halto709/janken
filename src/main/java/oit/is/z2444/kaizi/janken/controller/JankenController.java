4755b01a8d8166a5ed802a379945e4503b9d4458
Halto709 <e1b22071@oit.ac.jp>
2024-10-07 00:23:14 +0900

    じゃんけんの処理をmodelに分割

--- src/main/java/oit/is/z2444/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2444/kaizi/janken/controller/JankenController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Random;
+import oit.is.z2444.kaizi.janken.model.Janken;
 
 @Controller
 public class JankenController {
@@ -36,46 +36,10 @@ public class JankenController {
    */
   @GetMapping("/jankengame")
   public String jankengame(@RequestParam String hand, ModelMap model) {
-    Random random = new Random();
-    String playerHand = hand;
-    String enemyHand = "";
-    String jankenResult = "";
-
-    // cpuの手を生成
-    int enemy = random.nextInt(3);
-    switch (enemy) {
-      case 0:
-        enemyHand = "Gu";
-        break;
-      case 1:
-        enemyHand = "Choki";
-        break;
-      case 2:
-        enemyHand = "Pa";
-        break;
-      default:
-        break;
-    }
-
-    // 勝敗処理
-    if (playerHand.equals(enemyHand)) {
-      jankenResult = "Draw!";
-    }
-    if ((playerHand.equals("Gu") && enemyHand.equals("Choki"))
-        || (playerHand.equals("Choki") && enemyHand.equals("Pa"))
-        || (playerHand.equals("Pa") & enemyHand.equals("Gu"))) {
-      jankenResult = "You Win!";
-    }
-    if ((playerHand.equals("Gu") && enemyHand.equals("Pa"))
-        || (playerHand.equals("Choki") && enemyHand.equals("Gu"))
-        || (playerHand.equals("Pa") && enemyHand.equals("Choki"))) {
-      jankenResult = "You Lose...";
-    }
+    Janken janken = new Janken(hand);
 
     // それぞれの情報を格納
-    model.addAttribute("playerHand", playerHand);
-    model.addAttribute("enemyHand", enemyHand);
-    model.addAttribute("jankenResult", jankenResult);
+    model.addAttribute("janken", janken);
     return "janken.html";
   }
 
